e3bcbbf150e8f2fc2a4325ccb41918fd93fbf8c5
MateusMaiaDS <mmateusmaiads@gmail.com>
2022-07-04 13:45:19 +0100

    Adding dummy function

--- R/hello.R
+++ R/hello.R
@@ -1,18 +1,27 @@
-# Hello, world!
-#
-# This is an example function named 'hello' 
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Install Package:           'Cmd + Shift + B'
-#   Check Package:             'Cmd + Shift + E'
-#   Test Package:              'Cmd + Shift + T'
+#' @useDynLib cppbart
+#' @importFrom Rcpp sourceCpp
 
-hello <- function() {
-  print("Hello, world!")
+# Calling the function in the R version
+#' @export
+r_bart <- function(x,
+                   y,
+                   n_tree,
+                   n_mcmc,
+                   n_burn,
+                   n_min_size,
+                   tau, mu,
+                   a_tau,d_tau, tau_mu,
+                   alpha, beta){
+
+  # Getting the main function
+  bart_obj <- bart(x = x,
+                   y = y,
+                   n_tree = n_tree,
+                   n_mcmc = n_mcmc,
+                   n_burn = n_burn,
+                   n_min_size = n_min_size,
+                   tau = tau, mu = mu,
+                   a_tau = a_tau,d_tau = d_tau, tau_mu = tau_mu,
+                   alpha = alpha, beta = beta)
+  return(bart_obj)
 }
